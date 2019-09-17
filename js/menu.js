4ff118bff67847d00517c50f5a0665fc0b42ac92
cristian <cristianalexande@hotmail.com>
2019-09-17 18:11:07 -0500

    version 1

---
+++ js/menu.js
@@ -0,0 +1,5 @@
+var btnmenu = document.getElementById('btn-menu');
+var idnav = document.getElementById('idnav');
+btnmenu.addEventListener('click', function(){
+    idnav.classList.toggle('muestrate');
+})
